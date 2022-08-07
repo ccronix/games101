432679f8b7891933d8c3f61c57f9dbc967851389
cronix <cronix0325@163.com>
2022-08-07 19:11:21 +0800

    hw3 code reconstruct

--- Assignment3/code/Shader.hpp
+++ Assignment3/code/Shader.hpp
@@ -31,4 +31,19 @@ struct vertex_shader_payload
     Eigen::Vector3f position;
 };
 
+struct light
+{
+    Eigen::Vector3f position;
+    Eigen::Vector3f intensity;
+};
+
 #endif //RASTERIZER_SHADER_H
+
+Eigen::Vector3f vertex_shader(const vertex_shader_payload& payload);
+Eigen::Vector3f normal_fragment_shader(const fragment_shader_payload& payload);
+Eigen::Vector3f phong_fragment_shader(const fragment_shader_payload& payload);
+Eigen::Vector3f texture_fragment_shader(const fragment_shader_payload& payload);
+Eigen::Vector3f displacement_fragment_shader(const fragment_shader_payload& payload);
+Eigen::Vector3f bump_fragment_shader(const fragment_shader_payload& payload);
+
+static Eigen::Vector3f reflect(const Eigen::Vector3f& vec, const Eigen::Vector3f& axis);
