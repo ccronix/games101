8aca824f77649e6ba34e0fff3332a55e52d06103
cronix <cronix0325@163.com>
2022-08-30 21:23:41 +0800

    optimize render module

--- Assignment7/code/Renderer.cpp
+++ Assignment7/code/Renderer.cpp
@@ -2,6 +2,7 @@
 // Created by goksu on 2/25/20.
 //
 
+#include <omp.h>
 #include <fstream>
 #include "Scene.hpp"
 #include "Renderer.hpp"
@@ -14,26 +15,26 @@ const float EPSILON = 0.00001;
 // The main render function. This where we iterate over all pixels in the image,
 // generate primary rays and cast these rays into the scene. The content of the
 // framebuffer is saved to a file.
-void Renderer::Render(const Scene& scene)
+void Renderer::Render(const Scene& scene, const int spp)
 {
     std::vector<Vector3f> framebuffer(scene.width * scene.height);
 
     float scale = tan(deg2rad(scene.fov * 0.5));
     float imageAspectRatio = scene.width / (float)scene.height;
-    Vector3f eye_pos(278, 273, -800);
+    Vector3f eye_pos(0, 5, -18);
     int m = 0;
 
     // change the spp value to change sample ammount
-    int spp = 16;
+    // int spp = 16;
     std::cout << "SPP: " << spp << "\n";
-    for (uint32_t j = 0; j < scene.height; ++j) {
-        for (uint32_t i = 0; i < scene.width; ++i) {
+    for (int j = 0; j < scene.height; ++j) {
+        for (int i = 0; i < scene.width; ++i) {
             // generate primary ray direction
-            float x = (2 * (i + 0.5) / (float)scene.width - 1) *
-                      imageAspectRatio * scale;
+            float x = (2 * (i + 0.5) / (float)scene.width - 1) * scale * imageAspectRatio;
             float y = (1 - 2 * (j + 0.5) / (float)scene.height) * scale;
 
             Vector3f dir = normalize(Vector3f(-x, y, 1));
+            #pragma omp parallel for
             for (int k = 0; k < spp; k++){
                 framebuffer[m] += scene.castRay(Ray(eye_pos, dir), 0) / spp;  
             }
