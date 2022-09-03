6d3c08773f8f1400ad6a8dfd0c6753773cbf9ea4
cronix <cronix0325@163.com>
2022-09-03 22:57:39 +0800

    use validColor function to aviod NaN black color

--- Assignment7/code/Renderer.hpp
+++ Assignment7/code/Renderer.hpp
@@ -17,5 +17,6 @@ class Renderer
 public:
     void Render(const Scene& scene, const int spp = 16);
     bool writeImage(const int width, const int height, const std::vector<Vector3f> framebuffer, const char* path = "binary.ppm");
+    Vector3f validColor(const Vector3f pixel_color);
 private:
 };
