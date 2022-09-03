bc045d6d36a09abdf12ad7b45975009b1557d601
cronix <cronix0325@163.com>
2022-09-03 22:57:04 +0800

    modify scene color, code reformat, fix time counter bug.

--- Assignment7/code/main.cpp
+++ Assignment7/code/main.cpp
@@ -17,23 +17,26 @@ int main(int argc, char* argv[])
     Scene scene(1024, 1024);
 
     Material* red = new Material(DIFFUSE, Vector3f(0.0f));
-    red->Kd = Vector3f(0.75f, 0.25f, 0.25f);
+    red->Kd = Vector3f(0.6f, 0.05f, 0.05f);
     Material* green = new Material(DIFFUSE, Vector3f(0.0f));
-    green->Kd = Vector3f(0.25f, 0.25f, 0.75f);
+    green->Kd = Vector3f(0.15f, 0.45f, 0.1f);
     Material* white = new Material(DIFFUSE, Vector3f(0.0f));
     white->Kd = Vector3f(0.75, 0.75, 0.75);
-    Material* light = new Material(DIFFUSE, (42.0 * Vector3f(0.9, 0.75, 0.6)));
+    Material* reflect = new Material(MIRROR, Vector3f(0.0f));
+    reflect->Kd = Vector3f(0.0f, 0.0f, 0.0f);
+    reflect->ior = 40.0;
+    Material* light = new Material(DIFFUSE, (45.0 * Vector3f(0.9, 0.8, 0.7)));
     light->Kd = Vector3f(0.65f);
 
-    MeshTriangle front("cornell/pFront.obj", white);
+    // MeshTriangle front("cornell/pFront.obj", white);
     MeshTriangle floor("cornell/pBack.obj", white);
     MeshTriangle shortbox("cornell/boxSmall.obj", white);
-    MeshTriangle tallbox("cornell/boxTall.obj", white);
+    MeshTriangle tallbox("cornell/boxTall.obj", reflect);
     MeshTriangle left("cornell/pLeft.obj", red);
     MeshTriangle right("cornell/pRight.obj", green);
     MeshTriangle light_("cornell/light.obj", light);
 
-    scene.Add(&front);
+    // scene.Add(&front);
     scene.Add(&floor);
     scene.Add(&shortbox);
     scene.Add(&tallbox);
@@ -49,10 +52,22 @@ int main(int argc, char* argv[])
     r.Render(scene, 16);
     auto stop = std::chrono::system_clock::now();
 
+    int hours = std::chrono::duration_cast<std::chrono::hours>(stop - start).count();
+
+    int minutes = std::chrono::duration_cast<std::chrono::minutes>(stop - start).count();
+    if (hours > 0) {
+        minutes -= hours * 60;
+    }
+
+    int seconds = std::chrono::duration_cast<std::chrono::seconds>(stop - start).count();
+    if (minutes > 0) {
+        seconds -= hours * 3600 + minutes * 60;
+    }
+
     std::cout << "Render complete: \n";
-    std::cout << "Time taken: " << std::chrono::duration_cast<std::chrono::hours>(stop - start).count() << " hours\n";
-    std::cout << "          : " << std::chrono::duration_cast<std::chrono::minutes>(stop - start).count() << " minutes\n";
-    std::cout << "          : " << std::chrono::duration_cast<std::chrono::seconds>(stop - start).count() << " seconds\n";
+    std::cout << "Time taken: " << hours << " hours\n";
+    std::cout << "          : " << minutes << " minutes\n";
+    std::cout << "          : " << seconds << " seconds\n";
 
     return 0;
 }
